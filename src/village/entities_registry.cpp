40530ea6e67ac1d799b36b69ac5900bdd3a39008
unknown <playstation4love@gmail.com>
2025-12-18 13:14:15 +0200

    Made a tools window with initial price

---
+++ src/village/entities_registry.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Vadim Grabovenko on 10/12/2025.
+//
+
+#include "Entities_Registry.hpp"
+
+namespace vsa {
+namespace village {
+} // village
+} // vsa
---
+++ src/village/entities_registry.hpp
@@ -0,0 +1,18 @@
+//
+// Created by Vadim Grabovenko on 10/12/2025.
+//
+
+#ifndef ENTITIES_REGISTRY_HPP
+#define ENTITIES_REGISTRY_HPP
+
+namespace vsa {
+namespace village {
+
+class Entities_Registry {
+
+};
+
+} // village
+} // vsa
+
+#endif //ENTITIES_REGISTRY_HPP
